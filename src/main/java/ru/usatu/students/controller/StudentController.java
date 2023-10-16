9465998283d5e2d92812fe569d4c2ca0a9afc15a
Popil Roman <popil.roman23@mail.ru>
2023-10-16 14:01:46 +0500

    Add class for processing REST-request

---
+++ src/main/java/ru/usatu/students/controller/StudentController.java
@@ -0,0 +1,38 @@
+package ru.usatu.students.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.usatu.students.model.Student;
+import ru.usatu.students.service.StudentServiceCollection;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private StudentServiceCollection studentService;
+    private StudentController(StudentServiceCollection studentService) {this.studentService = studentService;}
+
+    @GetMapping
+    public @ResponseBody
+    List<Student> getStudents() {return studentService.getStudents();}
+
+    @GetMapping("/id")
+    public @ResponseBody
+    Student getStudent(@PathVariable int id) {return studentService.getStudent(id);}
+
+    @PostMapping
+    public @ResponseBody
+    Student addStudent(@RequestBody Student student) {return studentService.addStudent(student);}
+
+    @PutMapping("/id")
+    public @ResponseBody
+    Student editStudent(@PathVariable int id, @RequestBody String name) {return studentService.editStudent(id, name);}
+
+    @DeleteMapping("/id")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteStudent(@PathVariable int id) {studentService.deleteStudent(id);}
+
+
+
+}
