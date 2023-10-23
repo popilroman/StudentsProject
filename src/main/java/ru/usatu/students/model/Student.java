989161f1132640f20753b16734a64d0e5d24ab91
Popil Roman <popil.roman23@mail.ru>
2023-10-23 13:21:13 +0500

    In student.java has been added annotations for linking java-objects with xml

--- src/main/java/ru/usatu/students/model/Student.java
+++ src/main/java/ru/usatu/students/model/Student.java
@@ -1,7 +1,14 @@
 package ru.usatu.students.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Student {
+    @XmlElement(name = "id")
     private int id;
+    @XmlElement(name = "name")
     private String name;
 
     public Student() {}
