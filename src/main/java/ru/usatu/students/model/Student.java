f8cc6b7e353e4ae1f9a8671b7e68596abe66d2d1
Popil Roman <popil.roman23@mail.ru>
2023-10-24 07:44:52 +0500

    In Student.java has been added jpa-annotation to define the stored classes and entities of DB table

--- src/main/java/ru/usatu/students/model/Student.java
+++ src/main/java/ru/usatu/students/model/Student.java
@@ -1,13 +1,21 @@
 package ru.usatu.students.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 
+@Entity
+@Table(name = "students")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Student {
+    @Id
     @XmlElement(name = "id")
     private int id;
+    @Column(name = "name")
     @XmlElement(name = "name")
     private String name;
 
